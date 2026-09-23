896b01f843a8ed1211c705358d206658c1cde83b
Khamilovv0 <anayat.khamilov@bk.ru>
2026-09-23 12:07:09 +0500

    Исправлен вывод Тутора

--- app/Http/Controllers/backend/AllDatabaseController.php
+++ app/Http/Controllers/backend/AllDatabaseController.php
@@ -15,6 +15,40 @@ class AllDatabaseController extends Controller
         $this->middleware('auth');
     }
 
+    private function attachTutorNames($items)
+    {
+        $tutorIds = $items->pluck('TutorID')
+            ->merge($items->pluck('redactor_id'))
+            ->filter()
+            ->unique()
+            ->values()
+            ->toArray();
+
+        $tutors = DB::connection('mysql_platonus')
+            ->table('tutors')
+            ->whereIn('TutorID', $tutorIds)
+            ->select('TutorID', 'lastname', 'firstname')
+            ->get()
+            ->keyBy('TutorID');
+
+        $items->transform(function ($item) use ($tutors) {
+            $tutor = $tutors->get($item->TutorID);
+            $redactor = $tutors->get($item->redactor_id);
+
+            $item->tutor_fullname = $tutor
+                ? trim($tutor->lastname . ' ' . $tutor->firstname)
+                : '';
+
+            $item->redactor_fullname = $redactor
+                ? trim($redactor->lastname . ' ' . $redactor->firstname)
+                : '';
+
+            return $item;
+        });
+
+        return $items;
+    }
+
     public function all(Request $request)
     {
         $user = Auth::user();
@@ -26,17 +60,13 @@ class AllDatabaseController extends Controller
             ->leftJoin('auditories', 'in_product_lists.auditoryID', '=', 'auditories.auditoryID')
             ->leftJoin('buildings', 'in_product_lists.buildingID', '=', 'buildings.buildingID')
             ->leftJoin('in_product_name', 'in_product_lists.id_name', '=', 'in_product_name.id_name')
-            ->leftJoin('tutors AS tutor', 'in_product_lists.TutorID', '=', 'tutor.TutorID')
-            ->leftJoin('tutors AS redactor', 'in_product_lists.redactor_id', '=', 'redactor.TutorID')
             ->leftJoin('notes', 'in_product_lists.id_product', '=', 'notes.id_product')
             ->select(
                 'in_product_lists.*',
                 'buildings.buildingName',
                 'auditories.auditoryName',
                 'in_product_name.name_product',
-                'notes.note',
-                DB::raw("CONCAT(tutor.lastname, ' ', tutor.firstname) AS tutor_fullname"),
-                DB::raw("CONCAT(redactor.lastname, ' ', redactor.firstname) AS redactor_fullname")
+                'notes.note'
             )
             ->where('in_product_lists.actual_inventory', 1)
             ->where('in_product_lists.write_off', 1);
@@ -45,7 +75,6 @@ class AllDatabaseController extends Controller
             $query->where('in_product_lists.TutorID', $user->TutorID);
         }
 
-        // Фильтры
         if ($request->filled('buildingID')) {
             $query->where('in_product_lists.buildingID', $request->buildingID);
         }
@@ -66,15 +95,18 @@ class AllDatabaseController extends Controller
         }
 
         $items = $query->orderBy('id_product', 'desc')->get();
+        $items = $this->attachTutorNames($items);
 
         // Списки для фильтров
         $buildings = DB::connection('mysql_platonus')->table('buildings')->get();
         $auditories = DB::table('auditories')->orderByRaw('CAST(auditoryName AS UNSIGNED) asc')->get();
         $productNames = DB::table('in_product_name')->get();
-        $tutors = $isAdmin ? DB::connection('mysql_platonus')->table('tutors')->get() : collect();
+        $tutorsList = $isAdmin ? DB::connection('mysql_platonus')->table('tutors')->get() : collect();
 
-        return view('backend.invertory.create_invertory.all_db', compact('items', 'buildings', 'auditories', 'productNames', 'tutors'));
+        return view('backend.invertory.create_invertory.all_db', compact('items', 'buildings', 'auditories', 'productNames'))
+            ->with('tutors', $tutorsList);
     }
+
 
     public function filter(Request $request)
     {
@@ -87,17 +119,13 @@ class AllDatabaseController extends Controller
             ->leftJoin('auditories', 'in_product_lists.auditoryID', '=', 'auditories.auditoryID')
             ->leftJoin('buildings', 'in_product_lists.buildingID', '=', 'buildings.buildingID')
             ->leftJoin('in_product_name', 'in_product_lists.id_name', '=', 'in_product_name.id_name')
-            ->leftJoin('tutors AS tutor', 'in_product_lists.TutorID', '=', 'tutor.TutorID')
-            ->leftJoin('tutors AS redactor', 'in_product_lists.redactor_id', '=', 'redactor.TutorID')
             ->leftJoin('notes', 'in_product_lists.id_product', '=', 'notes.id_product')
             ->select(
                 'in_product_lists.*',
                 'buildings.buildingName',
                 'auditories.auditoryName',
                 'in_product_name.name_product',
-                'notes.note',
-                DB::raw("CONCAT(tutor.lastname, ' ', tutor.firstname) AS tutor_fullname"),
-                DB::raw("CONCAT(redactor.lastname, ' ', redactor.firstname) AS redactor_fullname")
+                'notes.note'
             )
             ->where('in_product_lists.actual_inventory', 1)
             ->where('in_product_lists.write_off', 1)
@@ -110,6 +138,7 @@ class AllDatabaseController extends Controller
         }
 
         $items = $query->orderBy('id_product', 'desc')->get();
+        $items = $this->attachTutorNames($items);
 
         $buildings = DB::connection('mysql_platonus')->table('buildings')->get();
         $auditories = DB::table('auditories')->orderByRaw('CAST(auditoryName AS UNSIGNED) asc')->get();
@@ -130,17 +159,13 @@ class AllDatabaseController extends Controller
             ->leftJoin('auditories', 'in_product_lists.auditoryID', '=', 'auditories.auditoryID')
             ->leftJoin('buildings', 'in_product_lists.buildingID', '=', 'buildings.buildingID')
             ->leftJoin('in_product_name', 'in_product_lists.id_name', '=', 'in_product_name.id_name')
-            ->leftJoin('tutors AS tutor', 'in_product_lists.TutorID', '=', 'tutor.TutorID')
-            ->leftJoin('tutors AS redactor', 'in_product_lists.redactor_id', '=', 'redactor.TutorID')
             ->leftJoin('notes', 'in_product_lists.id_product', '=', 'notes.id_product')
             ->select(
                 'in_product_lists.*',
                 'buildings.buildingName',
                 'auditories.auditoryName',
                 'in_product_name.name_product',
-                'notes.note',
-                DB::raw("CONCAT(tutor.lastname, ' ', tutor.firstname) AS tutor_fullname"),
-                DB::raw("CONCAT(redactor.lastname, ' ', redactor.firstname) AS redactor_fullname")
+                'notes.note'
             )
             ->where('in_product_lists.actual_inventory', 1)
             ->where('in_product_lists.write_off', 1)
@@ -155,6 +180,7 @@ class AllDatabaseController extends Controller
         }
 
         $items = $query->orderBy('id_product', 'desc')->get();
+        $items = $this->attachTutorNames($items);
 
         $buildings = DB::connection('mysql_platonus')->table('buildings')->get();
         $auditories = DB::table('auditories')->orderByRaw('CAST(auditoryName AS UNSIGNED) asc')->get();
@@ -175,17 +201,13 @@ class AllDatabaseController extends Controller
             ->leftJoin('auditories', 'in_product_lists.auditoryID', '=', 'auditories.auditoryID')
             ->leftJoin('buildings', 'in_product_lists.buildingID', '=', 'buildings.buildingID')
             ->leftJoin('in_product_name', 'in_product_lists.id_name', '=', 'in_product_name.id_name')
-            ->leftJoin('tutors AS tutor', 'in_product_lists.TutorID', '=', 'tutor.TutorID')
-            ->leftJoin('tutors AS redactor', 'in_product_lists.redactor_id', '=', 'redactor.TutorID')
             ->leftJoin('notes', 'in_product_lists.id_product', '=', 'notes.id_product')
             ->select(
                 'in_product_lists.*',
                 'buildings.buildingName',
                 'auditories.auditoryName',
                 'in_product_name.name_product',
-                'notes.note',
-                DB::raw("CONCAT(tutor.lastname, ' ', tutor.firstname) AS tutor_fullname"),
-                DB::raw("CONCAT(redactor.lastname, ' ', redactor.firstname) AS redactor_fullname")
+                'notes.note'
             )
             ->where('in_product_lists.actual_inventory', 1)
             ->where('in_product_lists.write_off', 1)
@@ -196,6 +218,7 @@ class AllDatabaseController extends Controller
         }
 
         $items = $query->orderBy('id_product', 'desc')->get();
+        $items = $this->attachTutorNames($items);
 
         $buildings = DB::connection('mysql_platonus')->table('buildings')->get();
         $auditories = DB::table('auditories')->orderByRaw('CAST(auditoryName AS UNSIGNED) asc')->get();
